0ffd4db6015462e0189368e96173cfd733695d05
tjncgit <tamajongnn@icloud.com>
2020-05-28 18:33:48 -0400

    replace random message with random personal facts

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['born in cameroon','Born in 2000','Favorite movie transformers'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
