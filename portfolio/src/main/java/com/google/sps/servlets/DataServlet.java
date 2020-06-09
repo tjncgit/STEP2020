0f6cfe61d9d35a156bd5666ace59e8cdd10c828b
tjncgit <tamajongnn@icloud.com>
2020-06-09 23:02:18 +0000

    Need to let user select the number of comments they want to display

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -21,6 +21,7 @@ import com.google.gson.Gson;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.FetchOptions;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -33,7 +34,6 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
   private ArrayList<String> comments;
-
   @Override
   public void init() {
     comments = new ArrayList<String>();
@@ -41,25 +41,29 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    int max = maxComments(request);
+
     Query query = new Query("Comment").addSort("comment", SortDirection.DESCENDING);
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     PreparedQuery results = datastore.prepare(query);
 
     ArrayList<String> comments = new ArrayList<String>();
-    for (Entity commentEntity : results.asIterable()) {
+    for (Entity commentEntity : results.asIterable())
+     {
       String comment = (String) commentEntity.getProperty("comment");
       comments.add(comment);
     }
-    
+
     String json = convertToJsonUsingGson(comments);
-    response.setContentType("application/json;");
+    response.setContentType("application/json");
     response.getWriter().println(json);
   }
 
 @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Get the input from the form.
+    int max = maxComments(request);
     String text = getParameter(request, "input-comment", "");
 
     Entity commentEntity = new Entity("Comment");
@@ -67,10 +71,24 @@ public class DataServlet extends HttpServlet {
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     datastore.put(commentEntity);
-
     // Redirect back to the HTML page.
+    response.getWriter().println(max);
     response.sendRedirect("/index.html");
   }
+
+  private int maxComments(HttpServletRequest request) throws IOException {
+      String maxCommentsString = getParameter(request, "max-comments","");
+      int maxComments;
+      try {
+       maxComments = Integer.parseInt(maxCommentsString);
+    } catch (NumberFormatException e) {
+      System.err.println("Could not convert to int: " + maxCommentsString);
+      return -1;
+    }
+
+    return maxComments;
+  }
+
 
   private String convertToJsonUsingGson(ArrayList<String> commentList) {
     Gson gson = new Gson();
@@ -89,5 +107,8 @@ public class DataServlet extends HttpServlet {
     }
     return value;
   }
-}
+    }
+
+
+
 
