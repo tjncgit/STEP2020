f06455de41114752970e5683ec2542b43637dfdf
tjncgit <tamajongnn@icloud.com>
2020-06-10 18:04:30 +0000

    Adds Servlet that returns login status of user

---
+++ portfolio/src/main/java/com/google/sps/servlets/Users.java
@@ -0,0 +1,80 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.gson.Gson;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Users")
+public class Users extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("text/html;");
+    response.getWriter().println("<h1>Toxic Comment Section</h1>");
+
+    // Only logged-in users can see the form
+    UserService userService = UserServiceFactory.getUserService();
+    if (userService.isUserLoggedIn()) {
+      response.getWriter().println("<p>Hello " + userService.getCurrentUser().getEmail() + "!</p>");
+      response.getWriter().println("<p>Type a message and click submit:</p>");
+      response.getWriter().println("<form method=\"POST\" action=\"/data\">");
+      response.getWriter().println("<input type=\"number\" name=\"max-comments\" id=\"param1\"><br/>");
+      response.getWriter().println("<input type=\"text\" name=\"input-comment\" id=\"param2\">");
+      response.getWriter().println("<br/>");
+      response.getWriter().println("<button>Submit</button>");
+      response.getWriter().println("</form>");
+
+      String userEmail = userService.getCurrentUser().getEmail();
+      String urlToRedirectToAfterUserLogsOut = "/index.html";
+      String logoutUrl = userService.createLogoutURL(urlToRedirectToAfterUserLogsOut);
+
+      response.getWriter().println("<p>Logout <a href=\"" + logoutUrl + "\">here</a>.</p>");
+    } else {
+      String loginUrl = userService.createLoginURL("/Users");
+
+    //   response.getWriter().println("<p>Hello stranger.</p>");
+    //   response.getWriter().println("<p>Login <a href=\"" + loginUrl + "\">here</a>.</p>");
+      response.sendRedirect(loginUrl);
+    }
+
+    // Everybody can see the messages
+    response.getWriter().println("<ul>");
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Query query = new Query("Message").addSort("timestamp", SortDirection.DESCENDING);
+    PreparedQuery results = datastore.prepare(query);
+    for (Entity entity : results.asIterable()) {
+      String text = (String) entity.getProperty("text");
+      String email = (String) entity.getProperty("email");
+      response.getWriter().println("<li>" + email + ": " + text + "</li>");
+    }
+    response.getWriter().println("</ul>");
+    }
+ }
+
+
